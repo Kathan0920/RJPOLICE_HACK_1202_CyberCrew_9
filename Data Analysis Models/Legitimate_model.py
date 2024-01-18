3f984bc49cc96bc77f2a24132b855cbb2004f936
Kathan <shahkathan50@gmail.com>
2024-01-18 09:31:50 +0530

    added ppt

--- Data Analysis Models/Legitimate_model.py
+++ Data Analysis Models/Legitimate_model.py
@@ -55,7 +55,7 @@ pipeline = Pipeline(steps=[('preprocessor', preprocessor),
                              ('classifier', model)])
 
 # Split the data into training and test sets
-X_train, X_test, y_train, y_test = atrain_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Train the model
 pipeline.fit(X_train, y_train)
